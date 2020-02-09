0c73869d2db6f25a70a4bb1c6d6e3b001398d73a
Maksim Baranovskyi <maximys2607@gmail.com>
2020-02-09 13:04:01 +0200

    corected hw-4

--- goit-js-hw-04/js/task-2.js
+++ goit-js-hw-04/js/task-2.js
@@ -1,26 +1,28 @@
 const inventory = {
-    items: ['Knife', 'Gas mask'],
-    add(itemName) {
-        console.log(`Adding ${itemName} to inventory`);
-      this.items.push(itemName);
-    },
-    remove(itemName) {
-      this.items = this.items.filter(item => item !== itemName);
-    },
-  };
-
-const invokeInventoryAction = function (itemName, inventoryAction) {
-  console.log(`Invoking ${inventoryAction.name} action on ${itemName}`);
-  inventoryAction.call(inventory, itemName);
+  items: ["Knife", "Gas mask"],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    this.items = this.items.filter(item => item !== itemName);
+  }
 };
- 
-invokeInventoryAction('Medkit', inventory.add);
+
+const invokeInventoryAction = function(itemName, inventoryAction) {
+  console.log(`Invoking ${inventoryAction.name} action on ${itemName}`);
+
+  const ivent =  inventoryAction.bind(inventory, itemName);
+  ivent();
+};
+
+invokeInventoryAction("Medkit", inventory.add);
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction('Gas mask', inventory.remove);
+invokeInventoryAction("Gas mask", inventory.remove);
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
