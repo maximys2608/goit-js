0457499e737b68ea24897a03d02e57e6e040f888
Maksim Baranovskyi <maximys2607@gmail.com>
2020-03-01 21:19:00 +0200

    home-work-6

---
+++ goit-js-hw-06/js/main.js
@@ -0,0 +1,99 @@
+import users from "/js/users.js";
+//1
+//Получить массив имен всех пользователей (поле name).
+const getUserNames = users => {
+  return users.map(user => user.name);
+};
+
+console.table(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+//2
+//Получить массив объектов пользователей по цвету глаз (поле eyeColor).
+
+const getUsersWithEyeColor = (users, color) => {
+  return users.filter(user => user.eyeColor === color).map(user => user.name);
+};
+
+console.table(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+
+//3
+//Получить массив имен пользователей по полу (поле gender).
+const getUsersWithGender = (users, gender) => {
+  return users.filter(user => user.gender === gender).map(user => user.name);
+};
+
+console.table(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+
+//4
+//Получить массив только неактивных пользователей (поле isActive).
+const getInactiveUsers = users => {
+  return users.filter( user => !user.isActive).map(user => user.name);
+};
+
+console.table(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+
+//5
+//Получить пользоваля (не массив) по email (поле email, он уникальный).
+const getUserWithEmail = (users, email) => {
+  return users.find( user => user.email === email);
+};
+
+console.table(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+console.table(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+
+//6
+//Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age).
+const getUsersWithAge = (users, min, max) => {
+  return users.filter(user => user.age >= min && user.age <= max);
+};
+
+console.table(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+console.table(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+
+//7
+//Получить общую сумму баланса (поле balance) всех пользователей.
+
+const calculateTotalBalance = users => {
+  return users.reduce((acc, user) => acc + user.balance, 0);
+};
+
+console.log(calculateTotalBalance(users)); // 20916
+
+//8
+//Массив имен всех пользователей у которых есть друг с указанным именем.
+
+const getUsersWithFriend = (users, friendName) => {
+  return users.filter( user => user["friends"].includes(friendName)).map(user => user.name);
+};
+
+console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+
+//9
+//Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
+
+const getNamesSortedByFriendsCount = users => {
+  const sortByfriends = (userOne, userTwo) => userOne["friends"].length - userTwo["friends"].length;
+  return users.sort(sortByfriends);
+};
+
+console.table(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+//10
+//Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
+
+const getSortedUniqueSkills = users => {
+  let skillsNotUnique = users
+.reduce((acc, {skills}) => [...acc, ...skills], []);
+
+skillsNotUnique = skillsNotUnique.sort();
+
+return [...new Set(skillsNotUnique)];
+};
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nost
